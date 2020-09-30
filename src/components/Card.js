c81374a4f0451a13de84faa73801cc32885987fd
srojasz <srojasz@gmail.com>
2020-09-30 20:04:35 +0200

    add styles

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,10 +5,15 @@ function Card(props) {
   const { id, card, icon, isOpen, onClick } = props;
 
   let name = isOpen ? props.name : "";
+  const cardUp = isOpen ? "card-up" : "";
 
   return (
     <React.Fragment>
-      <div onClick={onClick} id={id} className={`game-board__card ${card}`}>
+      <div
+        onClick={onClick}
+        id={id}
+        className={`game-board__card ${card} ${cardUp}`}
+      >
         <h3 className="game-board__card--title">{name}</h3>
         <i className={`game-board__card--icon ${icon}`}></i>
       </div>
