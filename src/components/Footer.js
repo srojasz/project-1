788be558064929167b86691e80f6aa46a245f947
srojasz <srojasz@gmail.com>
2020-10-01 06:35:58 +0200

    Add footer styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,16 +1,29 @@
-import React from 'react'
-import "../stylesheets/footer.scss"
+import React from "react";
+import "../stylesheets/footer.scss";
 
 function Footer() {
+  const cvUrl =
+    "https://drive.google.com/file/d/1E437fkUNZA5SwoeoCGXs12KNXBVxtsLE/view";
   return (
     <footer className="footer">
-      <p className="footer__text">
-        ¡Me encanta tu idea, pero no tengo tiempo! ¿Me lo cuentas del tirón?</p>
-      <button className="footer__button">
-        Sara, ¡vamos al grano!
-      </button>
+      <section className="footer__info">
+        <p className="footer__info--text">
+          ¡Me encanta tu idea, pero no tengo tiempo! ¿Me lo cuentas del tirón?
+        </p>
+        <a
+          href={cvUrl}
+          target="_blank"
+          title="Descarga el CV en PDF"
+          className="footer__info--button"
+        >
+          Sara, ¡vamos al grano!
+        </a>
+      </section>
+      <section className="footer__social">
+        <p>Aquí van las redes sociales</p>
+      </section>
     </footer>
-  )
+  );
 }
 
 export default Footer;
