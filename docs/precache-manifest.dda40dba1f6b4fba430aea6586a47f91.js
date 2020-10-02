d8f894095795617b7cd8516b1f92735d0ba44406
srojasz <srojasz@gmail.com>
2020-10-02 09:51:46 +0200

    create docs folder

---
+++ docs/precache-manifest.dda40dba1f6b4fba430aea6586a47f91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c83ef092ca84ed5a17755051c498c47",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9291a58ae21a9492442a",
+    "url": "./static/css/main.be6735f3.chunk.css"
+  },
+  {
+    "revision": "1a8e82642f81f563d17f",
+    "url": "./static/js/2.049df8d1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.049df8d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9291a58ae21a9492442a",
+    "url": "./static/js/main.9a36ec34.chunk.js"
+  },
+  {
+    "revision": "ca8051aa2fe5b3ef84c3",
+    "url": "./static/js/runtime-main.673723db.js"
+  }
+]);
